0b16811bc2d48a827d7ee44e5aacbffc579ae0d7
Branislav Valasek <branislav.valasek@infor.com>
2022-11-21 13:50:48 +0100

    connect to database

--- server/index.js
+++ server/index.js
@@ -8,3 +8,11 @@ const app = express();
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
 app.use(cors());
+
+// connecting to Mongo db
+const CONNECTION_URL = 'mongodb+srv://mario:mario@mernapp.qvzkmdm.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 4000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => app.listen(PORT, () => console.log('Server running on port: ', {PORT})))
+.catch((error) => console.log(error.message));
