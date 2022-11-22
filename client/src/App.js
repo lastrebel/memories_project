0f4ea27e97f18d6a12f582c5ea8d169610aafd7a
Branislav Valasek <branislav.valasek@infor.com>
2022-11-22 18:36:53 +0100

    Styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
 import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
 
-import Post from './components/Posts/Posts'
+import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
 import memories from './images/memories.png'
+import useStyles from './styles'
 
 const App = () => {
+const classes = useStyles()
+
     return (
         <Container maxwidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center" >Memories</Typography>
-                <img src={memories} alt="memories" heigth="60" />
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center" >Memories</Typography>
+                <img className={classes.image} src={memories} alt="memories" height="60px" />
             </AppBar>
             <Grow in>
                 <Container>
-                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} xm={7}>
-                            <Post />
+                    <Grid container justifyContent="space-between" alignItems="stretch" spacing={2}>
+                        <Grid item>
+                            <Posts />
                         </Grid>
-                        <Grid item xs={12} xm={4}>
+                        <Grid item>
                             <Form />
                         </Grid>
                     </Grid>
